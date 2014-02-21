f3bb523b0ebf6da63d98ca021a0c1ea1633ab6d0
Vilson Lu <vilson_lu@yahoo,com>
2014-02-21 23:41:58 +0800

    Very crude login validation

---
+++ LakbayRilesPH/Login.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LakbayRilesPH
+{
+    public class Login
+    {
+        public static string GreetUser(string name){
+            return "Welcome " + name;
+        }
+    }
+}
