a530ba40ff0ad5f179dd488bc965c3754e8cce24
Vilson Lu <vilson_lu@yahoo,com>
2014-02-25 21:04:47 +0800

    Eto gumagana na ung insert sa admin (yey)

---
+++ LakbayRilesPH/Admin.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebMatrix.Data;
+
+namespace LakbayRilesPH
+{
+    public class Admin
+    {
+        public static Boolean UpdateDatabase(int button)
+        {
+            return query(button);
+        }
+
+        private static Boolean query(int status)
+        {
+            var db = Database.Open("SQLAzureConnection");
+            var updateQuery = "INSERT INTO StationStatus (CodeID, StationID, Direction) VALUES (@0, @1, @2) ";
+            var time = DateTime.Now;
+            var sqlFormattedDate = time.Date.ToString("yyyy-MM-dd HH:mm:ss");
+            var flag = db.Execute(updateQuery, status, "1", "NORTH");
+            
+            if (flag != 0)
+                return true;
+            else
+                return false;
+
+        }
+    }
+}
