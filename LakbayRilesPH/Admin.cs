1527fae4ac31d4f401848da9013219ce55522404
Vilson Lu <vilson_lu@yahoo,com>
2014-02-26 21:33:44 +0800

    Completed the minimum function of admin. Wala pang mga alerts and stuffs.

--- LakbayRilesPH/Admin.cs
+++ LakbayRilesPH/Admin.cs
@@ -8,18 +8,18 @@ namespace LakbayRilesPH
 {
     public class Admin
     {
-        public static Boolean UpdateDatabase(int button)
+        public static Boolean UpdateDatabase(int button, string direction)
         {
-            return query(button);
+            return query(button, direction);
         }
 
-        private static Boolean query(int status)
+        private static Boolean query(int status, string direction)
         {
             var db = Database.Open("SQLAzureConnection");
             var updateQuery = "INSERT INTO StationStatus (CodeID, StationID, Direction) VALUES (@0, @1, @2) ";
             var time = DateTime.Now;
             var sqlFormattedDate = time.Date.ToString("yyyy-MM-dd HH:mm:ss");
-            var flag = db.Execute(updateQuery, status, "1", "NORTH");
+            var flag = db.Execute(updateQuery, status, "1", direction);
             
             if (flag != 0)
                 return true;
