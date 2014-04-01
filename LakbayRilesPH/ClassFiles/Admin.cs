a691b5b3ee262c44707415707f18603ff1ffaa64
Vilson Lu <vilson_lu@yahoo.com>
2014-04-02 01:28:25 +0800

    Station Announcement is now working. Yey!

--- LakbayRilesPH/ClassFiles/Admin.cs
+++ LakbayRilesPH/ClassFiles/Admin.cs
@@ -13,13 +13,34 @@ namespace LakbayRilesPH
             return query(button, direction, id);
         }
 
+        public static Boolean UpdateDatabase(string title, string message, int stationID)
+        {
+            return query(title, message, stationID);
+        }
+
+        private static Boolean query(string title, string message, int stationID)
+        {
+            var db = Database.Open("SQLAzureConnection");
+            var updateQuery = "INSERT INTO StationAnnouncement(AnnouncementTitle, AnnouncementBody, Station, AnnouncementTime) VALUES(@0, @1, @2, @3)";
+            var time = DateTime.Now;
+            var sqlFormattedDate = time.ToString("yyyy-MM-dd HH:mm:ss");
+
+            var flag = db.Execute(updateQuery, title, message, stationID, sqlFormattedDate);
+
+            if (flag != 0)
+                return true;
+            else
+                return false;
+            
+        }
+
         private static Boolean query(int status, string direction, int id)
         {
             var db = Database.Open("SQLAzureConnection");
             var updateQuery = "INSERT INTO StationStatus (Status, Supervisor, Direction, StatusTime) VALUES (@0, @1, @2, @3) ";
             var time = DateTime.Now;
             var sqlFormattedDate = time.ToString("yyyy-MM-dd HH:mm:ss");
-            // Don't forget to change these once the login module has been created.
+   
             var flag = db.Execute(updateQuery, status, id, direction, sqlFormattedDate);
             
             if (flag != 0)
---
+++ LakbayRilesPH/ClassFiles/Announcement.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LakbayRilesPH.ClassFiles
+{
+    public class Announcement
+    {
+        public string Title { get; set; }
+        public string Message { get; set; }
+        public DateTime Time { get; set; }
+    }
+}
